9a9a70f2fdfa47e3a44c6ffa80e91ab4c854d8c7
LinkWu <wjianl@qq.com>
2025-03-14 21:53:11 +0800

    删除特殊测试代码

--- test/parity_test.c
+++ test/parity_test.c
@@ -20,34 +20,5 @@ int main(int argc, char** argv) {
 
     if(parity_check(Odd, data, 1, &data[1])) { printf("check passed\n"); }
 
-    if(1 < argc && 0 == strcmp("send", *(argv + 1))) {
-        FILE* src = fopen("/home/link/workspace_git/ctree/main", "r");
-        FILE* dst = fopen("/home/link/workspace_git/ctree/main_send", "w");
-
-        fseek(src, 0, SEEK_END);
-        long     sz   = ftell(src);
-        uint8_t* buff = (uint8_t*)malloc(sz + 1);
-        fseek(src, 0, SEEK_SET);
-        fread(buff, 1, sz, src);
-        parity_calc(Odd, buff, sz + 1, NULL);
-
-        fwrite(buff, 1, sz + 1, dst);
-
-        fclose(src);
-        fclose(dst);
-    }
-    if(1 < argc && 0 == strcmp("recv", *(argv + 1))) {
-        FILE* dst = fopen("/home/link/workspace_git/ctree/main_send", "r");
-        fseek(dst, 0, SEEK_END);
-        long     sz_1 = ftell(dst);
-        uint8_t* buff = (uint8_t*)malloc(sz_1);
-        fseek(dst, 0, SEEK_SET);
-        fread(buff, 1, sz_1, dst);
-
-        if(parity_check(Odd, buff, sz_1, NULL)) { printf("passed\n"); }
-
-        fclose(dst);
-    }
-
     return EXIT_SUCCESS;
 }
